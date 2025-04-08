5689e1d2784fbe6efd86c0a5e4783ccdd8c28ce3
iagosoares0212 <iagosoares55@gmail.com>
2025-04-08 14:32:21 -0300

    desenvolvendo questão 2

--- src/index.ts
+++ src/index.ts
@@ -34,8 +34,15 @@ export const exercicio01 = (livros: Livro[], anoLimite: number): string[] => liv
 
 
 export const exercicio02 = (livros: Livro[], copiasDisponiveis: Livro['copiasDisponiveis'], genero: GeneroDeLivro): Omit<Livro, 'avaliacoes'>[] => livros
-.filter(livro => livro.copiasDisponiveis == copiasDisponiveis)
-.map(livros )
+    .filter(livro => livro.copiasDisponiveis == copiasDisponiveis)
+    .map(livros =>  {
+        const {avaliacoes, ...livroSemAvaliacoes} = livros;
+
+        return {
+            ...livroSemAvaliacoes,
+            generos: Array.from(new Set([...livros.generos, genero]))
+        };
+    });
 
 // export const exercicio03 = (livros: Livro[], autores: Autor[]): Record<string, string> => autores
 
