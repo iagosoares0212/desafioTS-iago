9a47fde224ac6b5e2cdf173599ef322f93765136
iagosoares0212 <iagosoares55@gmail.com>
2025-04-11 15:03:06 -0300

    entendendo exercício 1 e 2

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // Tipos
-interface Livro { // criação de uma interface para um livro, que é o molde, que define como o objeto de ser
+interface Livro { // criação de uma interface para um livro, que é o molde, que define como o objeto deve ser
     id: number;
     titulo: string;
     autorId: number;
     paginas: number;
-    generos: string[]; //pq não colocou generos: GeneroDeLivro[]; ??
+    generos: GeneroDeLivro[]; 
     avaliacoes: Avaliacoes[];
     copiasDisponiveis: number;
     anoPublicacao: number;
@@ -16,33 +16,28 @@ interface Avaliacoes { //criação de uma interface para o valor "Avaliacoes[]"
     nota: number;
 }
 
-type GeneroDeLivro = string // perguntar aos caras
+type GeneroDeLivro = string //Cria um apelido (alias) chamado GeneroDeLivro para o tipo primitivo string.
 
 // Resolução dos exercícios
 
 export const exercicio01 = (livros: Livro[], anoLimite: number): string[] => livros
-.filter(livro => livro.anoPublicacao < anoLimite )
+.filter(livro => livro.anoPublicacao < anoLimite ) 
 .filter(livro => {
     const totalNotas = livro.avaliacoes.reduce((soma, avaliacao) => soma + avaliacao.nota, 0)
     const media =  totalNotas / livro.avaliacoes.length
     return media >= 4.0
 })
-.map(livro => livro.titulo)
+.map(livro => livro.titulo) //retorna  apenas os titulos do objeto livro
 
 
-export const exercicio02 = ( 
-    livros: Livro[],
-    copiasDisponiveis: Livro['copiasDisponiveis'],
-    genero: GeneroDeLivro
-  ): Omit<Livro, 'avaliacoes'>[] =>
-    livros
-      .filter(livro => livro.copiasDisponiveis === copiasDisponiveis)
+export const exercicio02 = (livros: Livro[], copiasDisponiveis: Livro['copiasDisponiveis'], genero: GeneroDeLivro ): Omit<Livro, 'avaliacoes'>[] => livros
+      .filter(livro => livro.copiasDisponiveis === copiasDisponiveis) //filtrando os livros com base no número de cópias
       .map(livro => {
-        const { avaliacoes, ...livroSemAvaliacoes } = livro;
+        const { avaliacoes, ...livroSemAvaliacoes } = livro; //uso da desestruturação para separar o campo `avaliacoes` (que vai ser descartado) e Guardar o restante do livro em `livroSemAvaliacoes`
   
-        return {
-          ...livroSemAvaliacoes,
-          generos: [...livro.generos, genero] // apenas adiciona o gênero
+        return { //retorna um novo objeto que contém:
+          ...livroSemAvaliacoes, //todos os campos de LivroSemAvaliacoes
+          generos: [...livro.generos, genero] // substitui o campo `generos` do objeto LivroSemAvaliacoes por todos os generos que o objeto LivroSemAvaliacoes já tinha mais um novo gênero passado como parâmetro 
         };
 
         // ou fazer assim para remover duplicatas de gênero
@@ -52,6 +47,8 @@ export const exercicio02 = (
         //   };
     
       });
+
+//parei aqui :)
 
 // export const exercicio03 = (livros: Livro[], autores: Autor[]): Record<string, string> => autores
 
